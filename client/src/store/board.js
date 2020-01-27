fad14b36d8c67c2cbb1076d85866206fcf7aa833
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-27 00:38:26 -0300

    first commit

---
+++ client/src/store/board.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-return-assign */
+const state = {
+  droppingList: null,
+  draggingCard: null,
+};
+
+const mutations = {
+  setDroppingList: (state, droppingList) => state.droppingList = droppingList,
+  setDraggingCard: (state, draggingCard) => state.draggingCard = draggingCard,
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+};
