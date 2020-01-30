ef63730530c574856ac4c0f611a2649e6bcd3719
Marcelo Kabbalah <marceloregis.soares@gmail.com>
2020-01-29 23:51:15 -0300

    feat: add routers, Login, Board and Boards.vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,27 +1,58 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Router from 'vue-router';
 import Home from '../views/Home.vue';
 import SignUp from '../views/SignUp.vue';
+import Login from '../views/Login.vue';
+import Boards from '../views/Boards.vue';
+import Board from '../views/Board.vue';
 
-Vue.use(VueRouter);
+import store from '../store';
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/signup',
-    name: 'signup',
-    componente: SignUp,
-  },
-];
+Vue.use(Router);
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+function isLoggedIn(to, from, next) {
+  store.dispatch('auth/authenticate').then(() => {
+    next();
+  }).catch(() => {
+    next('/login');
+  });
+}
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      beforeEnter(to, from, next) {
+        store.dispatch('auth/authenticate').then(() => {
+          next('/boards');
+        }).catch(() => {
+          next('/login');
+        });
+      },
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/boards',
+      name: 'boards',
+      component: Boards,
+      beforeEnter: isLoggedIn,
+    },
+    {
+      path: '/boards/:id',
+      name: 'board',
+      component: Board,
+      beforeEnter: isLoggedIn,
+    },
+  ],
 });
-
-export default router;
